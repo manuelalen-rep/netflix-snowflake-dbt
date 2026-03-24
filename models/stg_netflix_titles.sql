1ec0276d1d7792f40475f56326f08fa050c07881
Manuel Alén <manuelalen@protonmail.com>
2026-03-24 13:52:57 +0000

    commit de proyecto dbt para netflix

---
+++ models/stg_netflix_titles.sql
@@ -0,0 +1,28 @@
+WITH raw_data AS (
+    SELECT * FROM {{ source('raw', 'netflix_raw') }}
+)
+
+SELECT
+    show_id,
+    type,
+    title,
+    CASE 
+        WHEN duration ILIKE '%min%' THEN TRY_CAST(REGEXP_SUBSTR(duration, '(\\d+)') AS INT)
+        ELSE NULL 
+    END as duration_minutes,
+    
+    CASE 
+        WHEN duration ILIKE '%Season%' THEN TRY_CAST(REGEXP_SUBSTR(duration, '(\\d+)') AS INT)
+        ELSE NULL 
+    END as duration_seasons,
+    COALESCE(
+        TRY_TO_DATE(TRIM(date_added, ' "'), 'MMMM DD, YYYY'),
+        TRY_TO_DATE(TRIM(date_added, ' "'), 'MON DD, YYYY'),
+        TRY_TO_DATE(TRIM(date_added, ' "')) 
+    ) as date_added_clean,
+    
+    release_year,
+    rating,
+    listed_in as categories
+
+FROM raw_data
